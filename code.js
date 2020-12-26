5109897b3cf8bd576078f8dce9a4118d975f77f8
gjnmbm <gjnmbm@gmail.com>
2020-12-26 13:16:27 -0800

    Add method to access file info

--- code.js
+++ code.js
@@ -6,7 +6,7 @@ let hours = 0; //= minutes/60;
 function changeTimestamp() {
     milliSec += 1;
     //Isn't there a simpler way to do this? A less lines way?
-    //I will need to change how the timestamp changes later on
+    //I will need to change how the timestamp changes later on and make sure that I don't start the timestamp at 0
     if (milliSec == 60) {
         milliSec = 0;
         seconds += 1;
@@ -20,4 +20,14 @@ function changeTimestamp() {
         hours += 1;
     }
     document.getElementById('timestamp').textContent = `${hours.toFixed(0)}:${minutes.toFixed(0)}:${seconds.toFixed(0)}:${milliSec.toFixed(0)}`;
+}
+
+//With this function I'm able to get information on the file provided by the user
+//The info is printed to the console
+function getFileInfo() {
+    const fileSelector = document.getElementById('fileSelect');
+    fileSelector.addEventListener('change', (event) => {
+        const fileList = event.target.files;
+        console.log(fileList);
+  });
 }
