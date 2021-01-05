e6ea7ebfdd26f9ce1ab041dff6d2ebe3bdb479fa
gjnmbm <gjnmbm@gmail.com>
2021-01-04 20:00:27 -0800

    Add framerate and edit play

--- code.js
+++ code.js
@@ -30,26 +30,24 @@ function getAudioDuration() { //This is to see if I'm able to get the duration o
 }
 
 function playAudio() { //Audio is never NULL
-    if (typeof audio === 'undefined') {
-        document.getElementById("errorMsg").textContent = "Couldn't load audio";
-    }
-    else {
-        //Testing to see if loaded audio will play
-        //Might need the interval function to check if
-        //audio has reached the end of it's playback portion
-        audio.play(); //This is a function for audio files
-        setTimeout(audio.stop(), end); //Don't know if this will work yet
-    }
+    //Testing to see if loaded audio will play
+    //Might need the interval function to check if
+    //audio has reached the end of it's playback portion
+    document.getElementById("errorMsg").textContent = "";
+    audio.play(); //This is a function for audio files
+    setTimeout(audio.stop(), end); //Don't know if this will work yet
 }
 
 function setAudioURL() { //This will load the audio and I'll be able to access it from the global
 //variable
     let file = document.getElementById('fileSelect');
+    //document.getElementById("errorMsg").textContent = ""; Only else statement would run
     audio = document.getElementById('audioSource');
     audio.src = URL.createObjectURL(file.files[0]);
     audioLength = audio.duration;
 }
 
 function setFrameRate() {
-    frameRate = document.getElementById(frameRate).value;
+    frameRate = parseInt(document.getElementById("frameRate").value, 10);
+    console.log(frameRate);
 }
